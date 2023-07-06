d6ec6142e951d0ee648784f4229d18a0f128c3bc
mrpetrakova2000 <mrpetrakova2000@gmail.com>
2023-07-06 11:43:56 +0300

    #106-graphql-femaleInMath

--- GraphQL/src/main/java/ru/etu/worldbankgraphql/WorldbankGraphqlApplication.java
+++ GraphQL/src/main/java/ru/etu/worldbankgraphql/WorldbankGraphqlApplication.java
@@ -1,4 +1,4 @@
-package ru.etu.worldbankgraphql.worldbankgraphql;
+package ru.etu.worldbankgraphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
